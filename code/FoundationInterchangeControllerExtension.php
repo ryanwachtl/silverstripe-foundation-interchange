9ceb9b845cb3197f1e74927d3a3ed14f19974d12
micschk <mic@restruct-web.nl>
2015-01-18 16:48:20 +0100

    Fixing #1: making paritals work on /home/ URLSegment
    Preventing the redirection from /home/ to / if requesting a partial

--- code/FoundationInterchangeControllerExtension.php
+++ code/FoundationInterchangeControllerExtension.php
@@ -9,7 +9,15 @@ class FoundationInterchangeControllerExtension extends Extension {
 		$vars = $controller->request->getVars();
 		$partial = (isset($vars[$this->template_param]) && $vars[$this->template_param] != "") ? $vars[$this->template_param] : false;
 		if ($partial) {
+			// render partial
 			$controller->response->setBody($controller->renderWith($partial));
+			// Prevent redirection of /home/ to / if requesting a partial of homepage, 
+			// should happen before $controller->response->output();
+			if($this->owner->URLSegment=='home'){
+				$this->owner->response->removeHeader('Location');
+				$this->owner->response->setStatusCode(200);
+			}
+			// output to browser
 			$controller->response->output();
 			exit();
 		}
